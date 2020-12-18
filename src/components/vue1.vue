790df2f063b22237571e857072b642290be63873
limingying <lmy123456>
2020-12-18 17:51:41 +0800

    关于js内容的练习3

---
+++ src/components/vue1.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="app">
+  <h2 v-text="message"></h2>
+  <h2 v-text="info"></h2>
+</div>
+</template>
+
+<script>
+export default {
+name: "vue1"
+}
+var app =new vue({
+  el :"#app",
+  data: {
+    message:"程序",
+    info:"前端",
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
